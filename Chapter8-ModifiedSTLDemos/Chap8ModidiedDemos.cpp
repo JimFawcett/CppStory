02294572805313cffb006c6ae2c5c1d3a1228c05
unknown <jfawcett@twcny.rr.com>
2020-01-22 13:57:21 -0500

    added stl code

--- Chapter8-ModifiedSTLDemos/Chap8ModidiedDemos.cpp
+++ Chapter8-ModifiedSTLDemos/Chap8ModidiedDemos.cpp
@@ -9,45 +9,36 @@
 #include <iostream>
 #include <functional>
 #include <algorithm>
+#include <type_traits>
 #include "../Chapter7-Display/Chap7Display.h"
 
-template<typename Cont, typename Op>
-void doProc(Cont c, Op op) {
-  std::for_each(c.begin(), c.end(), op);
-}
-
-template<typename Cont, typename Algo, typename Op>
-void doProc(Cont c, Algo algo, Op op) {
-  algo(c.begin(), c.end(), op);
-  //std::for_each(c.begin(), c.end(), op);
-}
-
-//template<typename V>
-//auto op = [](V v) {
-//  static bool first = true;
-//  if (first) {
-//    std::cout << "\n  " << v;
-//    first = false;
-//  }
-//  else {
-//    std::cout << ", " << v;
-//  }
-//};
-
 template<typename V>
-class Op {
+class ShowOp {
 public:
+  ShowOp(const std::string& prefix = ", ") : prefix_(prefix) {}
   void operator()(V v) {
     if (first) {
       std::cout << "\n  " << v;
       first = false;
     }
     else {
-      std::cout << ", " << v;
+      std::cout << prefix_ << v;
     }
   }
 private:
   bool first = true;
+  std::string prefix_ = ", ";
+};
+
+template<typename V>
+class SumOp {
+public:
+  void operator()(V v) {
+    sum_ += v;
+  }
+  V sum() { return sum_; }
+private:
+  V sum_;
 };
 
 template<typename C>
@@ -60,7 +51,7 @@ template<typename C>
 void show(C& c) {
   //putline(1, "  ");
   std::cout << "  ";
-  for_each(c.begin(), c.end(), Op<typename C::value_type>());
+  for_each(c.begin(), c.end(), ShowOp<typename C::value_type>());
 }
 
 template<typename T>
@@ -72,9 +63,12 @@ using Lst = std::list<T>;
 template<typename T>
 using iterator = typename std::vector<T>::iterator;
 
-template<typename C>
-struct Range {
-  Range(C& c, Op<typename C::value_type> op_ = nullptr) : op(op_) {
+template<typename O>
+struct Null {};
+
+template<typename C, template<typename> class Op = Null>
+struct Collection {
+  Collection(C& c, Op<typename C::value_type> op_ = Null<typename C::value_type>()) : op(op_) {
     first = c.begin();
     last = c.end();
   }
@@ -83,30 +77,41 @@ struct Range {
   Op<typename C::value_type> op;
 };
 
-template<typename C>
-void foreach(Range<C> rng) {
-  std::for_each(rng.first, rng.last, rng.op);
+template<typename C, template<typename> class Op>
+typename Op<typename C::value_type> foreach(Collection<C, Op> rng, Op<typename C::value_type> op) {
+  return std::for_each(rng.first, rng.last, op);
 }
 
-template<typename C, typename Op>
-void foreach(Range<C> rng, Op op) {
-  std::for_each(rng.first, rng.last, op);
+template<typename C, template<typename> class Op>
+typename Op<typename C::value_type> foreach(Collection<C> rng, Op<typename C::value_type> op) {
+  return std::for_each(rng.first, rng.last, op);
 }
 
-//template<typename Arg1, typename Arg2, typename Op>
-//const auto doFor = std::for_each<Arg1, Arg2, Op>;
+template<typename C, template<typename> class Op>
+typename Op<typename C::value_type> foreach(Collection<C, Op> rng) {
+  return std::for_each(rng.first, rng.last, rng.op);
+}
 
-//auto f = std::bind(&std::for_each, _1, _2, _3);
+
+//template<typename Cont, typename Op>
+//void doProc(Cont c, Op op) {
+//  std::for_each(c.begin(), c.end(), op);
+//}
+//
+//template<typename Cont, typename Algo, typename Op>
+//void doProc(Cont c, Algo algo, Op op) {
+//  algo(c.begin(), c.end(), op);
+//}
 
 int main() {
 
   displayDemo("-- traditional algo use --");
   Vec<int> vecInt{ 1, 2, 3, 4, 5 };
-  std::for_each(vecInt.begin(), vecInt.end(), Op<int>());
+  std::for_each(vecInt.begin(), vecInt.end(), ShowOp<int>());
 
   displayDemo("\n  -- using generic synonyms --");
   Vec<std::string> vecStr{ "Joe", "Sally", "Ashok", "Ming", "Hasim" };
-  Range<Vec<std::string>> rFriends(vecStr, Op<std::string>());
+  Collection<Vec<std::string>, ShowOp> rFriends(vecStr, ShowOp<std::string>());
 
   /*-- std algorithm with synonym arguments*/
 
@@ -118,15 +123,24 @@ int main() {
   
   /*-- prettified using range and separate operation --*/
 
-  Op<std::string> op;
-  auto showCout = [&op](const std::string& s) { op(s); };
+  //Op<std::string> op;
+  //auto showCout = [&op](const std::string& s) { op(s); };
+  ShowOp<std::string> showCout;
   foreach(rFriends, showCout);
+  putline();
+
+  Collection<Vec<int>, SumOp> vecIntColl(vecInt, SumOp<int>());
+  SumOp<int> ret = foreach(vecIntColl, SumOp<int>());
+  show(vecInt);
+  std::cout << "\n  sum = " << ret.sum();
+
+  //std::cout << "\n  sum = " << foreach<Collection<Vec<int>, SumOp<int>>(vecIntColl, SumOp<int>());
 
   displayDemo("\n  -- using synonyms --");
   auto first1 = vecInt.begin();
   auto last1 = vecInt.end();
   auto rng1 = std::pair{ first1, last1 };
-  auto slop1 = Op<int>();
+  auto slop1 = ShowOp<int>();
   auto forEachOne1 = [](decltype(first1) f, decltype(last1) l, decltype(slop1) s) {
     for_each(f, l, s);
   };
@@ -136,22 +150,33 @@ int main() {
   auto first2 = lstDbl.begin();
   auto last2 = lstDbl.end();
   auto rng2 = std::pair{ first2, last2 };
-  auto slop2 = Op<double>();
+  auto slop2 = ShowOp<double>();
   auto forEachOne2 = [](decltype(first2) f, decltype(last2) l, decltype(slop2) s) {
     for_each(f, l, s);
   };
   forEachOne2(rng2.first, rng2.second, slop2);
 
-
   displayDemo("\n  -- all the rest --");
   putline();
   Vec<double> vecDbl{ 1.0, -1.0, 0.5 };
-  std::list<int> listInt{ 1, 2, 3, 4, 5 };
-  doProc(vecInt, Op<int>());
-  doProc(vecDbl, Op<double>());
-  doProc(listInt, Op<int>());
+  Collection<Vec<double>, SumOp> vecDblColl(vecDbl, SumOp<double>());
+  
+  Collection<Vec<double>> vecDblColl2(vecDbl);
 
-  //doProc(vecInt, doFor<Vec<int>::iterator, Vec<int>::iterator, Op<int>>, Op<int>());
+  SumOp<double> retd = foreach(vecDblColl2, SumOp<double>());
+  double sumd = foreach(vecDblColl2, SumOp<double>()).sum();
+  show(vecDbl);
+  std::cout << "\n  sumd = " << retd.sum();
+
+  std::list<int> listInt{ 1, 2, 3, 2, 1 };
+  Collection<Lst<int>> listIntColl(listInt);
+  foreach(listIntColl, ShowOp<int>());
+  int sumi = foreach(listIntColl, SumOp<int>()).sum();
+  std::cout << "\n  sum of list = " << sumi;
+  //foreach(vecDbl, SumOp<double>());
+  //doProc(vecInt, ShowOp<int>());
+  //doProc(vecDbl, ShowOp<double>());
+  //doProc(listInt, ShowOp<int>());
 
   putline(2);
 }
